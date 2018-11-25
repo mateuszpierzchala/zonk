c349224200bc5113e9f5f124fcf755080b334b15
mateusz.pierzchala <mateusz.pierzchala1@gmail.com>
2018-11-25 16:11:28 +0100

    'dziala!'

--- learn.py
+++ learn.py
@@ -4,23 +4,25 @@ from keras.layers import Dense
 from keras.utils import np_utils
 import numpy
 
-data = numpy.loadtxt("shows.cvs", delimiter=",",skiprows=1)
+data = numpy.loadtxt("shows.csv", delimiter=",",skiprows=1)
 X = data[:,0:3]
 Y = data[:,3]
-
+print('oto data: \n', data)
+print(X)
+print(Y)
 # przeksztalcenie do postaci one hot notation
 categories = np_utils.to_categorical(Y)
-
+print(categories)
 model = Sequential()
-
 model.add(Dense(10, input_dim=3, activation = 'relu'))
-model.add(Dense(10, input_dim=3, activation = 'sigmoid'))
+model.add(Dense(3, activation = 'relu'))
+model.add(Dense(3, activation = 'sigmoid'))
 
 model.compile(loss='binary_crossentropy',optimizer = 'adam')
 model.fit(X, categories, epochs = 100, batch_size=100, verbose = 0)
 test_data = numpy.array([[0,1,2],[0,2,1], [1,0,2], [1,2,0], [2,0,1], [2,1,0]])
-
+print('test data', test_data)
 pred = model.predict(test_data)
-
+print(type(pred))
 for (idx, row) in enumerate(test_data):
-	print(idx,row)
+	print(row, row[2])
